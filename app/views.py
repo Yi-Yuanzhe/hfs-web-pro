fddd82a994634c4ff16eddca8adf32fa9a716c54
YiCorleone <yicorleone@gmail.com>
2016-10-02 22:54:59 +0800

    commit

--- app/views.py
+++ app/views.py
@@ -8,21 +8,8 @@ from app import hfs
 @app.route('/')
 @app.route('/index')
 def index():
-    user = { 'nickname': 'Miguel' }
-    posts = [
-        {
-            'author': { 'nickname': 'John' },
-            'body': 'Beautiful day in Portland!'
-            },
-            {
-            'author': { 'nickname': 'Susan' },
-            'body': 'The Avengers movie was so cool!'
-            }
-            ]
     return render_template('index.html',
-        title = 'Home',
-        user = user,
-        posts = posts)
+        title = 'Home')
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
